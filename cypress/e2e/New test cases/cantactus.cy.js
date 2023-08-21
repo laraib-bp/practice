0dee33e846b4d48da415d875b22dfd145ea82f49
laraib <laraib@bitspro.com>
2023-08-21 18:10:37 +0500

    test

---
+++ cypress/e2e/New test cases/cantactus.cy.js
@@ -0,0 +1,10 @@
+describe('should be able to submit a successful submission via contact us form', () => {
+    it('test contact us for via automation test store', () => {
+      cy.visit('https://automationteststore.com/')
+      cy.get("a[href$='contact']").click();
+      cy.get('#ContactUsFrm_first_name').type("joe");
+      cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+      cy.get('#ContactUsFrm_enquiry').type("do you provide additional discount on bulk orders")
+      cy.get('.col-md-6 > .btn').click();
+    });
+  }
---
+++ cypress/e2e/contact-us.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe("Test contact us from webddriveruni", () => ) {
+    it("should be able to submit a scuccesfull submission via contact us form", () =>{
+        //cypress code
+        cy.visit("http://www.webdriveruniversity.com/")
+    });
+
+    it("should not be able to submit a scuccesfull submission via contact us form as all fields are required//", () =>{
+        //cypress code
+    });
+})
+
